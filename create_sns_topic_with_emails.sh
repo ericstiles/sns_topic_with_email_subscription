fa663dfec5ca9b447d03a86a326b35e6ea7d2bb7
Eric <ets04uga@yahoo.com>
2020-02-21 11:10:00 -0600

    Update create_sns_topic_with_emails.sh

--- create_sns_topic_with_emails.sh
+++ create_sns_topic_with_emails.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for email in $sns_emails; do
-  echo "Subscribing to ${sns_arn} with ${email}"
+  echo "Subscribing to ${sns_topic_arn} with ${email}"
   aws sns subscribe --topic-arn "${sns_topic_arn}" --protocol email --notification-endpoint "${email}"
 done
