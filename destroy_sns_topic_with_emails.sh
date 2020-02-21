ce995f2b8763cf21580c3a2b081d9e0e5b8161da
Eric <ets04uga@yahoo.com>
2020-02-21 11:10:30 -0600

    Update destroy_sns_topic_with_emails.sh

--- destroy_sns_topic_with_emails.sh
+++ destroy_sns_topic_with_emails.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Deleting sns topic ${topic_arn}"
-aws sns delete-topic --topic-arn $topic_arn
+echo "Deleting sns topic ${sns_topic_arn}"
+aws sns delete-topic --topic-arn ${sns_topic_arn}
